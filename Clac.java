d3c3668e13d8d897d5a9bf6a58c7f1146bd6a139
Jack Proudfoot <jproudfoot2018@pingry.org>
2017-10-04 20:19:37 -0400

    Completed Framework
    Completed the basic framework for the clan assignment. Still need to
    add tests.

--- Clac.java
+++ Clac.java
@@ -1,10 +1,30 @@
 import java.util.Queue;
+import java.util.Stack;
 
 public class Clac {
-	public static boolean evaluate(Queue<String> tokenQueue) {
-		String token = tokenQueue.poll();
+	public static boolean evaluate(Queue<String> tokenQueue, Stack<Integer> stack, Stack<Queue<String>> state) {
+		String token;
 		
-		//
+		while(!tokenQueue.isEmpty()) {
+			token = tokenQueue.poll();
+			
+			//Print
+			if (token.equals("print")) {
+				int x = stack.pop(); //Handle Error
+				System.out.println(x);
+			} 
+		    
+			//Quit
+			else if (token.equals("quit")) {
+				return false;
+			} 
+			
+			else {
+				int number = Integer.parseInt(token); //Handle Error
+				stack.push(number);
+			}   
+		}
+		
 		return true;
 	}
 }
---
+++ ClacTests.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import org.junit.Test;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ClacTests {
+	Queue<String> tokenQueue;
+	Stack<Integer> stack;
+	Stack<Queue<String>> state;
+	Stack<Integer> resultStack;
+	
+	@Before
+	public void setup() {
+		stack = new Stack<Integer>();
+		state = new Stack<Queue<String>>();
+	}
+	
+	
+	@Test
+	public void a_testPrint_0() {
+		Logger.addTask(1, "(arithmetic expressions)");
+		Logger.addTest("print-0");
+		Logger.expectSuccess();
+		
+		tokenQueue = buildQueue("1 2 3 print");
+		resultStack = buildStack("1 2");
+		
+		Clac.evaluate(tokenQueue, stack, state);
+		
+		assertTrue(stack.equals(resultStack));
+	}
+	
+	
+	private static Queue<String> buildQueue(String code) {
+		Queue<String> tokenQueue = new LinkedList<String>();
+		
+		for (String tok : code.split(" ")) {
+			tokenQueue.offer(tok);
+		}
+		
+		return tokenQueue;
+	}
+	
+	private static Stack<Integer> buildStack(String stackString) {
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for (String tok : stackString.split(" ")) {
+			stack.push(Integer.parseInt(tok));
+		}
+		
+		return stack;
+	}
+}
