7f6d00fdc5aa09bda8269b1a5f6d0a935954d8ac
Jack Proudfoot <jproudfoot2018@pingry.org>
2017-10-14 20:21:39 -0400

    Assignment Stub
    Files included in the handout

--- Clac.java
+++ Clac.java
@@ -1,17 +1,22 @@
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Clac {
-	public static boolean evaluate(Queue<String> tokenQueue, Stack<Integer> stack, Stack<Queue<String>> state, Map<String, String> dict) {
+	public static boolean evaluate(Queue<String> tokenQueue, Stack<Integer> stack, Stack<Queue<String>> state, Map<String, Queue<String>> dict) {
 		String token;
 		
-		while(!tokenQueue.isEmpty()) {
+		while(!tokenQueue.isEmpty() || !state.isEmpty()) {
+			if (tokenQueue.isEmpty()) {
+				tokenQueue = state.pop();
+			}
+			
 			token = tokenQueue.poll();
 			
 			//Print
 			if (token.equals("print")) {
-				int x = stack.pop(); //Handle Error
+				int x = stack.pop();
 				System.out.println(x);
 			} 
 		    
@@ -20,12 +25,60 @@ public class Clac {
 				return false;
 			} 
 			
+			
+			
+			/* ************************************** */
+			/*   WRITE ALL OF YOUR TASK 1 CODE HERE   */
+			/* DO NOT MODIFY ANY CODE BELOW THIS AREA */
+			/* ************************************** */
+			
+			
+			//Function definition
+			else if (token.equals(":")) {
+				Queue<String> functionQueue = new LinkedList<String>();
+				
+				/* ******************************* *
+				/* FINISH FUNCTION DEFINITION HERE */
+				/* ******************************* */
+				
+				dict.put(functionQueue.poll(), functionQueue);
+				
+			}
+			
 			else {
-				int number = Integer.parseInt(token); //Handle Error
-				stack.push(number);
+				Queue<String> dictQueue = dict.get(token);
+				if (dictQueue != null) {
+					Queue<String> functionQueue = copyQueue(dictQueue);
+					Queue<String> currentQueue = copyQueue(tokenQueue);
+					if (!currentQueue.isEmpty()) state.push(currentQueue);
+					
+					tokenQueue = functionQueue;
+					 
+				}
+				else {
+					int number = Integer.parseInt(token); //Handle Error
+					stack.push(number);
+				}
 			}   
 		}
 		
 		return true;
+	}
+	
+	private static Queue<String> copyQueue(Queue<String> queue) {
+		Queue<String> newQueue = new LinkedList<String>();
+		Queue<String> oldQueue = new LinkedList<String>();
+		
+		while(!queue.isEmpty()) {
+			newQueue.offer(queue.peek());
+			oldQueue.offer(queue.poll());
+		}
+		
+		while(!oldQueue.isEmpty()) {
+			queue.offer(oldQueue.poll());
+		}
+		
+		
+		return newQueue;
 	}
 }
